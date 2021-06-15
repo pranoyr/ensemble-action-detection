b08760340953065a19ed4db4d00857417f30fb6f
Pranoy R <pranoyr@Pranoy-2.local>
2021-06-15 14:01:12 +0530

    updated:

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ def main():
 			state = {'epoch': epoch, 'model_state_dict': model.state_dict(),
 					'optimizer_state_dict': optimizer.state_dict(), 'scheduler_state_dict':scheduler.state_dict()}
 			if val_loss < th:
-				torch.save(state, os.path.join('./snapshots', 'ensemble-model.pth'))
+				torch.save(state, os.path.join('./snapshots', f'{opt.dataset}_model.pth'))
 				print("Epoch {} model saved!\n".format(epoch))
 				th = val_loss
 
