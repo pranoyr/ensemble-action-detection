a8e2bf556e5a1abc4c0bb5808c21a7184b3ae607
Pranoy R <pranoyr@Pranoy-2.local>
2021-06-16 12:59:17 +0530

    updated predict.py

--- predict.py
+++ predict.py
@@ -29,8 +29,6 @@ def main():
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
 
     # CUDA for PyTorch
     use_cuda = torch.cuda.is_available()
