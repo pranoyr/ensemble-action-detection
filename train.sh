ba9c061a2213d179a52287422040f767e9a1d118
Pranoy R <pranoyr@Pranoy-2.local>
2021-06-28 10:14:44 +0530

    jpeg to png

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python main.py --dataset_path /home/nvidia/data/Phoning-Smoking-Dataset --dataset Custom  --num_classes 2 --depth 18
+python main.py --dataset_path /home/neuroplex/data/Phoning-Smoking-Dataset --dataset Custom  --num_classes 2 --depth 18
