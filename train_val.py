180427b6a91cb9ca52d63ad928552c130167d244
Pranoy R <pranoyr@Pranoy-2.local>
2021-08-16 19:08:19 +0530

    test loss added

--- train_val.py
+++ train_val.py
@@ -140,7 +140,7 @@ def main():
 	summary_writer = tensorboardX.SummaryWriter(log_dir='tf_logs')
 	# define model
 	# model = ResidualNet("ImageNet", opt.depth, opt.num_classes, "CBAM")
-	model = torchvision.models.resnet18(pretrained=True, num_classes=opt.num_classes)
+	model = torchvision.models.resnet18(pretrained=True, num_classes=opt.num_classes, strict=False)
 	# model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=opt.num_classes)
 # 	model = ViT(
 #     image_size = 256,
